2ed875269a63d0aeecd8e968a90c55062da3e56e
matheusscheibel <131703589+matheusscheibel@users.noreply.github.com>
2023-09-08 02:18:21 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,58 @@
+<?php
+session_start();
+
+if (isset($_SESSION["email"])) {
+    // Usuário já está logado, redirecionar para a página principal
+    header("Location: principal.php");
+    exit();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+
+    // Verificar se o email já está cadastrado
+    $usuarios = file_exists("usuarios.txt") ? file("usuarios.txt", FILE_IGNORE_NEW_LINES) : [];
+
+    foreach ($usuarios as $linha) {
+        $dados = explode(",", $linha);
+        if ($dados[1] == $email) {
+            echo "<p>Email já cadastrado. Escolha outro email.</p>";
+            break;
+        }
+    }
+
+    // Se o email não estiver cadastrado, adicionar o novo usuário ao arquivo
+    if (!isset($dados) || $dados[1] != $email) {
+        $novoUsuario = "$nome,$email,$senha";
+        file_put_contents("usuarios.txt", $novoUsuario . PHP_EOL, FILE_APPEND);
+        echo "<p>Cadastro bem-sucedido como $email. <a href='login.php'>Faça login</a></p>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cadastro</title>
+</head>
+<body>
+    <h1>Cadastro</h1>
+
+    <!-- Formulário de cadastro -->
+    <h2>Inscreva-se</h2>
+    <form method="post" action="">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" required><br>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
