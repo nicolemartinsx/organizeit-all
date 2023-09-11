558fffbb31d092dc7cbde2ba6587903a1e915b8f
nicole <107952589+mrtnsx@users.noreply.github.com>
2023-09-11 15:05:36 -0300

    style login e cadastro

--- cadastro.php
+++ cadastro.php
@@ -34,26 +34,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300;400;500;600;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
-<body>
-    <h1>Cadastro</h1>
 
-    <!-- Formulario de cadastro -->
-    <h2>Inscreva-se</h2>
-    <form method="post" action="">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" required><br>
+<body class="cadastro">
+    <div>
+        <img class="logo" src="imagens/logo.png">
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" required><br>
+        <!-- Formulario de cadastro -->
+        <div class="container">
 
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" required><br>
+            <form method="post" action="">
+                <h1>cadastro</h1>
+                <input type="text" name="nome" placeholder="Nome" required>
 
-        <input type="submit" value="Cadastrar">
-    </form>
+                <input type="email" name="email" placeholder="Email" required>
+
+                <input type="password" name="senha" placeholder="Senha" required>
+
+                <button class="cadastrar">criar conta</button>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,56 +1,68 @@
-<?php
-session_start();
-
-if (isset($_SESSION["email"])) {
-    // Usuario j esta logado, redirecionar para a pagina principal
-    header("Location: principal.php");
-    exit();
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
-    $senha = $_POST["senha"];
-
-    // Verificar os dados do usuário no arquivo "usuarios.txt"
-    $usuarios = file("usuarios.txt", FILE_IGNORE_NEW_LINES);
-    foreach ($usuarios as $linha) {
-        $dados = explode(",", $linha);
-        if ($dados[1] == $email && $dados[2] == $senha) {
-            $_SESSION["email"] = $email;
-            $_SESSION["nome"] = $dados[0];
-            echo "<p>Login bem-sucedido como $email. Redirecionando para a página principal...</p>";
-            echo "<meta http-equiv='refresh' content='2;url=principal.php'>"; // Redireciona após 2 segundos
-            break;
-        }
-    }
-
-    // Se não for encontrado um usuario correspondente, exibe uma mensagem de erro
-    echo "<p>Email ou senha incorretos.</p>";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Signika:wght@300;400;500;600;700&family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
-    <h1>Login</h1>
+    <div class="container">
 
-    <!-- Formulario de login -->
-    <h2>Faça login</h2>
-    <form method="post" action="">
-        <label for="email">Email:</label>
-        <input type="email" name="email" required><br>
+        <img class="logologin" src="imagens/logologin.png">
+        <div class="campos">
+            <h1>iniciar sessão</h1>
 
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" required><br>
+            <!-- Formulario de login -->
+            <form method="post" action="">
+                <input type="email" name="email" placeholder="Email" required>
 
-        <input type="submit" value="Entrar">
-    </form>
+                <div class="botaologin">
+                    <input type="password" name="senha" placeholder="Senha" required>
 
-    <!-- Botão para redirecionar para a pagina de cadastro -->
-    <a href="cadastro.php">Novo Usuário</a>
+                    <button type="submit"><img src="imagens/entrarbutton.png"></button>
+                </div>
+                <?php
+                session_start();
+
+                if (isset($_SESSION["email"])) {
+                    // Usuario ja esta logado, redirecionar para a pagina principal
+                    header("Location: principal.php");
+                    exit();
+                }
+
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $email = $_POST["email"];
+                    $senha = $_POST["senha"];
+
+                    // Verificar os dados do usuário no arquivo "usuarios.txt"
+                    $usuarios = file("usuarios.txt", FILE_IGNORE_NEW_LINES);
+                    foreach ($usuarios as $linha) {
+                        $dados = explode(",", $linha);
+                        if ($dados[1] == $email && $dados[2] == $senha) {
+                            $_SESSION["email"] = $email;
+                            $_SESSION["nome"] = $dados[0];
+                            echo "<p>Login bem-sucedido como $email. Redirecionando para a página principal...</p>";
+                            echo "<meta http-equiv='refresh' content='2;url=principal.php'>"; // Redireciona após 2 segundos
+                            break;
+                        }
+                    }
+
+                    // Se não for encontrado um usuario correspondente, exibe uma mensagem de erro
+                    echo "<p>email ou senha incorretos!</p>";
+                }
+                ?>
+            </form>
+
+            <!-- Botão para redirecionar para a pagina de cadastro -->
+            <a href="cadastro.php">cadastre-se</a>
+        </div>
+    </div>
 </body>
+
 </html>
