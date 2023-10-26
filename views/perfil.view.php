599f07a3e7ef2e67b0f2d5088da7d78c50b02166
CelsonB <103614998+CelsonB@users.noreply.github.com>
2023-10-25 22:11:41 -0300

    Add files via upload

--- views/perfil.view.php
+++ views/perfil.view.php
@@ -1,16 +1,4 @@
-<?php require('header.php');
-
-
-$nomePerfil = $_SESSION['nome'];
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    foreach ($_POST['numero'] as $key => $value) {
-        echo $value;
-        $FilmesFav = "$nomePerfil,$value";
-        file_put_contents("../models/dados/watchlist.txt", $FilmesFav . PHP_EOL, FILE_APPEND);
-    }
-}
-?>
+<?php require('header.php');?>
 
 <body>
     <main>
@@ -24,56 +12,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <hr>
         </div>
         <?php
+        MostrarWatchlist();
+        
 
-        require('../models/filmes.model.php');
-        $foo = FALSE;
-        $favoritos = file_exists("../models/dados/watchlist.txt") ? file("../models/dados/watchlist.txt", FILE_IGNORE_NEW_LINES) : [];
-
-        foreach ($favoritos as $linha) {
-            $dados = explode(",", $linha);
-            if ($dados[0] == $nomePerfil) {
-                $foo = TRUE;
-
-                // echo "<p>";
-                // echo str_replace("_" , " ", $dados[1]);
-                foreach ($filmes as $filme) {
-
-                    if ($dados[1] == $filme['titulo']) { ?>
-                        <a href="../
-                        controllers/filme.controller.php?filme=<?= urlencode($filme['titulo']) ?>">
-                            <img src="<?= $base_url . '/' . $filme['capa'] ?>" class="capa" alt=<?= $filme['titulo'] ?>>
-                        </a>
-        <?php }
-                }
-
-                $foo = true;
-            }
-        }
         ?>
-        <?php if ($foo == false) { ?>
-            <h1>Você não tem nenhum filme na watchlist, clique e adicione</h1>
-            <div class="filmes">
-
-                <?php
-                $i = 0;
-
-                require("../models/inicial.model.php");
-                foreach ($filmes as $filme) {
-                ?>
-
-                    <a href="filme.controller.php?filme=<?= urlencode($filme['titulo']) ?>">
-                        <img src=<?= $base_url . '/' . $filme['capa'] ?> class="capa" alt=<?= $filme['titulo'] ?>>
-                    </a>
-
-                <?php
-                    $i++;
-                }
-                ?>
-
-
-
-            </div>
-        <?php } ?>
+        <?php  ?>
 
 
 
--- views/watchlist.view.php
+++ views/watchlist.view.php
@@ -1,17 +1,17 @@
 <?php require('header.php');
 
-if (isset($_GET["filme"]) && isset($_GET["watchlist"])) {
-    $watchlist = file_exists("../models/dados/watchlist.txt") ? file("../models/dados/watchlist.txt", FILE_IGNORE_NEW_LINES) : [];
-    $novo = [];
-    foreach ($watchlist as $linha) {
-        $dados = explode(",", $linha);
-        if ($dados[0] != $_SESSION["nome"] || $dados[1] != $_GET["filme"]) {
-            $novo[] = $linha;
-        }
-    }
-    $novo = implode(PHP_EOL, $novo);
-    file_put_contents("../models/dados/watchlist.txt", $novo);
-}
+//if (isset($_GET["filme"]) && isset($_GET["watchlist"])) {
+//    $watchlist = file_exists("../models/dados/watchlist.txt") ? file("../models/dados/watchlist.txt", FILE_IGNORE_NEW_LINES) : [];
+//    $novo = [];
+//    foreach ($watchlist as $linha) {
+//        $dados = explode(",", $linha);
+//        if ($dados[0] != $_SESSION["nome"] || $dados[1] != $_GET["filme"]) {
+//            $novo[] = $linha;
+//        }
+//    }
+//    $novo = implode(PHP_EOL, $novo);
+///    file_put_contents("../models/dados/watchlist.txt", $novo);
+//}
 ?>
 
 <body>
@@ -25,31 +25,9 @@ if (isset($_GET["filme"]) && isset($_GET["watchlist"])) {
         <div class="tabelareviews">
             <?php
 
-            require('../models/filmes.model.php');
+       
             $foo = FALSE;
-            $favoritos = file_exists("../models/dados/watchlist.txt") ? file("../models/dados/watchlist.txt", FILE_IGNORE_NEW_LINES) : [];
-
-            foreach ($favoritos as $linha) {
-                $dados = explode(",", $linha);
-                if ($dados[0] == $_SESSION['nome']) {
-                    $foo = TRUE;
-
-                    // echo "<p>";
-                    // echo str_replace("_" , " ", $dados[1]);
-                    foreach ($filmes as $filme) {
-                        if ($dados[1] == $filme['titulo']) { ?>
-                            <div class="filmecontainer">
-                                <a href="../controllers/filme.controller.php?filme=<?= urlencode($filme['titulo']) ?>">
-                                    <img src="<?= $base_url . '/' . $filme['capa'] ?>" class="capa" alt=<?= $filme['titulo'] ?>>
-                                </a>
-                                <a class="btfiltrar" href="../controllers/watchlist.controller.php?filme=<?= urlencode($filme['titulo']) ?>&watchlist=0">remover</a>
-                            </div>
-            <?php }
-                    }
-
-                    $foo = true;
-                }
-            }
+            $foo = MostrarWatchlist();
             ?>
         </div>
         <?php if ($foo == false) { ?>
@@ -57,19 +35,10 @@ if (isset($_GET["filme"]) && isset($_GET["watchlist"])) {
             <div class="filmes">
 
                 <?php
-                $i = 0;
+                MostrarFilmes();
+         
+               
 
-                require("../models/inicial.model.php");
-                foreach ($filmes as $filme) {
-                ?>
-
-                    <a href="filme.controller.php?filme=<?= urlencode($filme['titulo']) ?>">
-                        <img src=<?= $base_url . '/' . $filme['capa'] ?> class="capa" alt=<?= $filme['titulo'] ?>>
-                    </a>
-
-                <?php
-                    $i++;
-                }
                 ?>
 
 
