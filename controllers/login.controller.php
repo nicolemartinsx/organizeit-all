95799b123cd663af01ae9e007cd76922e470849e
CelsonB <103614998+CelsonB@users.noreply.github.com>
2023-10-28 17:31:36 -0300

    Add files via upload

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -1,4 +1,36 @@
 <?php
-
+require_once("../conexao.php");
 require("../models/login.model.php");
+session_start();
+
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+$bd = Conexao::get();
+
+$query = $bd->prepare("SELECT * FROM usuarios WHERE email = :email and senha = :senha");
+$query->bindParam(':email', $_POST['email']);
+$query->bindParam(':senha', $_POST['senha']);
+$query->execute();
+
+$result = $query->fetch();
+
+if($result){
+
+   $_SESSION["email"] = $_POST['email'];
+   $_SESSION["nome"] = $result['nome'];
+   $_SESSION['adm'] = 1;
+   $_SESSION['id'] = $result['id'];
+   echo'query foi um sucesso';
+   header("Location: ../index.php");
+}else{
+   echo 'não funcionou';
+}
+
+
+}
 require("../views/login.view.php");
