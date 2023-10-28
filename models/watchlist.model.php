be22e1e8dc972de25ee2765cb822ca3cf62e512e
CelsonB <103614998+CelsonB@users.noreply.github.com>
2023-10-28 20:30:16 -0300

    🗃️ Adição de uma Class Watchlist

---
+++ models/watchlist.model.php
@@ -0,0 +1,23 @@
+<?php
+
+class Watchlist
+{
+    private $titulo;
+    private $diretor;
+    private $estrelas;
+    private $genero;
+    private $ano;
+    private $capa;
+
+    public function __get($propriedade)
+    {
+        return $this->$propriedade;
+    }
+
+    public function __set($propriedade, $valor)
+    {
+        return $this->$propriedade = $valor;
+    }
+}
+
+
