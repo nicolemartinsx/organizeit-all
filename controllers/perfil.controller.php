66e6afa69199fa1b3e91d609b9662099867851bf
CelsonB <103614998+CelsonB@users.noreply.github.com>
2023-10-28 20:28:51 -0300

    ✨ Adição de Watchlist e ♻️ Routing

--- controllers/perfil.controller.php
+++ controllers/perfil.controller.php
@@ -1,50 +1,71 @@
 <?php
 
-require("../views/perfil.view.php");
-require_once("../conexao.php");
-$nomePerfil = $_SESSION['nome'];
+require_once("conexao.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+require("models/watchlist.model.php");
 
-    foreach ($_POST['numero'] as $key => $value) {
-        echo $value;
-        $FilmesFav = "$nomePerfil,$value";
-        file_put_contents("../models/dados/watchlist.txt", $FilmesFav . PHP_EOL, FILE_APPEND);
+
+
+
+class PerfilController {
+
+    function MostrarPerfil($idPerfil) {
+        
+        
+        require_once("conexao.php");
+        $bd = Conexao::get();
+        $query = $bd->prepare("SELECT * FROM watchlist LEFT JOIN filmes on watchlist.idfilmes = filmes.id where watchlist.idusuarios = :id; " );
+        $query->bindParam(':id', $idPerfil);
+        $query->execute();
+     
+        $watchlist= $query->fetchAll(PDO::FETCH_CLASS, "Watchlist");
+
+        require("views/perfil.view.php");
+
+        /* function MostrarWatchlist (){
+
+       
+        
+            require_once("conexao.php");
+            $bd = Conexao::get();
+            $query = $bd->prepare("SELECT * FROM watchlist LEFT JOIN filmes on watchlist.idfilmes = filmes.id where watchlist.idusuarios = :id; " );
+            $query->bindParam(':id', $_SESSION['id']);
+            $query->execute();
+         
+            $watchlist = $query->fetchAll(PDO::FETCH_CLASS, "Watchlist");
+        
+        
+                if ($watchlist!=false) {
+                    $foo = TRUE;
+        
+                    // echo "<p>";
+                    // echo str_replace("_" , " ", $dados[1]);
+                    foreach ($watchlist as $filme) :
+                        ?>
+                            <a href="filme.controller.php?id=<?= urlencode($filme->id) ?>">
+                                <img src="<?= 'data:image/jpeg;base64,' . base64_encode($filme->capa) ?>" class="capa" alt=<?= $filme->titulo ?>>
+                            </a>
+                        <?php
+                        endforeach;
+                    }
+        
+                    $foo = true;
+            
+                } */
+
+
     }
+
+
+   
+        
+
+
+
 }
 
-class Watchlist
-{
-}
-
-function MostrarWatchlist (){
- 
-  
-
-    $foo = FALSE;
-    require_once("../conexao.php");
-    $bd = Conexao::get();
-    $query = $bd->prepare("SELECT * FROM watchlist LEFT JOIN filmes on watchlist.idfilmes = filmes.id where watchlist.idusuarios = :id; " );
-    $query->bindParam(':id', $_SESSION['id']);
-    $query->execute();
- 
-    $watchlist = $query->fetchAll(PDO::FETCH_CLASS, "Watchlist");
 
 
-        if ($watchlist!=false) {
-            $foo = TRUE;
 
-            // echo "<p>";
-            // echo str_replace("_" , " ", $dados[1]);
-            foreach ($watchlist as $filme) :
-                ?>
-                    <a href="filme.controller.php?id=<?= urlencode($filme->id) ?>">
-                        <img src="<?= 'data:image/jpeg;base64,' . base64_encode($filme->capa) ?>" class="capa" alt=<?= $filme->titulo ?>>
-                    </a>
-                <?php
-                endforeach;
-            }
 
-            $foo = true;
-        }
-    
+
--- controllers/watchlist.controller.php
+++ controllers/watchlist.controller.php
@@ -1,59 +1,102 @@
 <?php
+//require_once("../conexao.php");
+require("models/watchlist.model.php");
+require("models/filme.model.php");
 
-require("../views/watchlist.view.php");
-require("../models/filme.model.php");
-require_once("../conexao.php");
-
-class watchlist{
-
-}
-function MostrarWatchlist (){
- 
-  
-
-    $foo = FALSE;
-    require_once("../conexao.php");
-    $bd = Conexao::get();
-    $query = $bd->prepare("SELECT * FROM watchlist LEFT JOIN filmes on watchlist.idfilmes = filmes.id where watchlist.idusuarios = :id; " );
-    $query->bindParam(':id', $_SESSION['id']);
-    $query->execute();
- 
-    $watchlist = $query->fetchAll(PDO::FETCH_CLASS, "Watchlist");
+    class WatchlistController{
 
 
-        if ($watchlist!=false) {
-            $foo = TRUE;
+        public function MostrarWatchlist(){
+            require('layout/header.php');
+            require_once("conexao.php");
+            $bd = Conexao::get();
+            $query = $bd->prepare("SELECT * FROM watchlist LEFT JOIN filmes on watchlist.idfilmes = filmes.id where watchlist.idusuarios = :id; " );
+            $query->bindParam(':id', $_SESSION['id']);
+            $query->execute();
+     
+            $watchlist= $query->fetchAll(PDO::FETCH_CLASS, "Watchlist");
 
-            // echo "<p>";
-            // echo str_replace("_" , " ", $dados[1]);
-            foreach ($watchlist as $filme) :
-                ?>
-                    <a href="filme.controller.php?id=<?= urlencode($filme->id) ?>">
-                        <img src="<?= 'data:image/jpeg;base64,' . base64_encode($filme->capa) ?>" class="capa" alt=<?= $filme->titulo ?>>
-                    </a>
-                <?php
-                endforeach;
-                $foo = true;
-                return $foo;
-            }
-             $foo = false;
-             return $foo;
-
-        }
-        
-        function MostrarFilmes (){
-            
+            if($watchlist==false){
+                $foo=false;
                 $bd = Conexao::get();
+                $query = $bd->prepare("SELECT * FROM filmes limit 5 " );
+                $query->execute();
+     
+                $filmes= $query->fetchAll(PDO::FETCH_CLASS, "Filme");
+            }else{
+                $foo=true;
+            }
+            
 
-                $query = $bd->prepare("SELECT * FROM filmes");
-                $query->execute();  
-                $filmes = $query->fetchAll(PDO::FETCH_CLASS, "Filme");
+        require("views/watchlist.view.php");
+        }
 
-                foreach ($filmes as $filme) :
+
+
+
+    }
+
+
+
+
+
+/* class WatchlistController{
+
+    function MostrarWatchlist (){
+
+        
+       
+        $foo = FALSE;
+        require_once("conexao.php");
+        $bd = Conexao::get();
+        $query = $bd->prepare("SELECT * FROM watchlist LEFT JOIN filmes on watchlist.idfilmes = filmes.id where watchlist.idusuarios = :id; " );
+        $query->bindParam(':id', $_SESSION['id']);
+        $query->execute();
+     
+        $watchlist = $query->fetchAll(PDO::FETCH_CLASS, "Watchlist");
+    
+    
+            if ($watchlist!=false) {
+                $foo = TRUE;
+    
+                // echo "<p>";
+                // echo str_replace("_" , " ", $dados[1]);
+                foreach ($watchlist as $filme) :
                     ?>
                         <a href="filme.controller.php?id=<?= urlencode($filme->id) ?>">
                             <img src="<?= 'data:image/jpeg;base64,' . base64_encode($filme->capa) ?>" class="capa" alt=<?= $filme->titulo ?>>
                         </a>
                     <?php
-                    endforeach;
-        }
+                endforeach;
+                   
+                }
+                 
+               
+                
+            }
+            
+            function MostrarFilmes (){
+                          
+                    $bd = Conexao::get();
+                    $query = $bd->prepare("SELECT * FROM filmes");
+                    $query->execute();  
+                    $filmes = $query->fetchAll(PDO::FETCH_CLASS, "Filme");
+    
+                    require("views/watchlist.view.php");   
+                    require("models/filme.model.php"); 
+            }
+
+
+} 
+
+
+ 
+class Filmes{
+
+}
+
+class Watchlist{
+
+}
+
+?> */
