673b7a9f417eaaca8452d799748cf0c58c31a1e7
CelsonB <103614998+CelsonB@users.noreply.github.com>
2023-10-28 20:32:38 -0300

    ♻️ Routing de Watchlist

--- index.php
+++ index.php
@@ -4,10 +4,21 @@ require("vendor/autoload.php");
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
 Router::get('/', 'InicialController@index');
+
+Router::get('/watchlist', 'WatchlistController@MostrarWatchlist');
+
 Router::get('/pesquisa', 'PesquisaController@index');
 Router::get('/em-alta', 'PesquisaController@emAlta');
 Router::get('/filme/cadastrar', 'CadastrarFilmeController@index');
 Router::post('/filme/cadastrar', 'CadastrarFilmeController@cadastrar');
 Router::get('/filme/{id}', 'FilmeController@index');
+Router::get('/filme/{id}', 'FilmeController@index');
+
+Router::get('/filme/{id}/adicionar', 'FilmeController@AddWatchlist');
+Router::get('/filme/{id}/remover', 'FilmeController@RemoverWatchlist');
+
+Router::get('/perfil/{id}', 'PerfilController@MostrarPerfil');
+
+Router::get('/watchlist', 'WatchlistController@MostrarWatchlist');
 
 Router::start();
