e17e5b8e2358d75e5106134c8d1f8c4e8fea1fbb
CelsonB <103614998+CelsonB@users.noreply.github.com>
2023-10-31 15:38:16 -0300

    💥 fix: Corrigido todos os users serem adm

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -10,28 +10,33 @@ class LoginController
    }
 
 
-   function RealizarLogin()
-   {
-      session_start();
-
-      $bd = Conexao::get();
-      $query = $bd->prepare("SELECT * FROM usuarios WHERE email = :email and senha = :senha");
-      $query->bindParam(':email', $_POST['email']);
-      $query->bindParam(':senha', $_POST['senha']);
-      $query->execute();
-
-      $result = $query->fetch();
-
-      if ($result) {
-
-         $_SESSION["email"] = $_POST['email'];
-         $_SESSION["nome"] = $result['nome'];
-         $_SESSION['adm'] = 1;
-         $_SESSION['id'] = $result['id'];
-         echo 'query foi um sucesso';
-         header("Location: /");
-      } else {
-         echo 'não funcionou';
-      }
+   function RealizarLogin(){
+    
+      
+    
+         session_start(); 
+     
+         $bd = Conexao::get();
+         $query = $bd->prepare("SELECT * FROM usuarios WHERE email = :email and senha = :senha");
+         $query->bindParam(':email', $_POST['email']);
+         $query->bindParam(':senha', $_POST['senha']);
+         $query->execute();
+         
+         $result = $query->fetch();
+         
+         if($result){
+         
+            $_SESSION["email"] = $_POST['email'];
+            $_SESSION["nome"] = $result['nome'];
+            $_SESSION['adm'] = $result['adm'];
+            $_SESSION['id'] = $result['id'];
+            echo'query foi um sucesso';
+            header("Location: /");
+         }else{
+            echo 'não funcionou';
+         }
+         
+         
+        
    }
 }
